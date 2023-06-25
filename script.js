84cabaf824a33c6afe3b846270343f4acfe4d917
Nicolò Taormina <nicktaormina3@gmail.com>
2023-06-25 18:29:14 +0200

    refactored code

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeToggle = document.getElementById('dark-mode-toggle-button');
+// Funzione per il cambio di tema
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    toggleDarkmodeIcons();
+}
+
+// Funzione per verificare il tema corrente e nascondere/lasciare visibili le icone appropriate
+function toggleDarkmodeIcons() {
     const body = document.body;
     const sunIcon = document.querySelector('.sun-icon');
     const moonIcon = document.querySelector('.moon-icon');
 
-    // Verifica il tema corrente e nasconde/lascia visibile l'icona appropriata
-    function toggleIcons() {
-        if (body.classList.contains('dark-mode')) {
-            sunIcon.style.display = 'none';
-            moonIcon.style.display = 'inline-block';
-        } else {
-            sunIcon.style.display = 'inline-block';
-            moonIcon.style.display = 'none';
-        }
+    if (body.classList.contains('dark-mode')) {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    } else {
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
     }
-
-    toggleIcons(); // Imposta le icone iniziali
-    setCopyright(); // Imposta il copyright
-    darkModeToggle.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        toggleIcons(); // Aggiorna le icone dopo il cambio di tema
-    });
-});
-
-function setCopyright() {
-    const footer = document.getElementById("footer");
-    const currentYear = new Date().getFullYear();
-    const startYear = 2022;
-    const name = "Nicolò Taormina";
-    footer.textContent = "\u00A9 " + startYear + "-" + currentYear + " " + name;
 }
 
+// Funzione per impostare il tema iniziale
+function setInitialTheme() {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDarkMode) {
+        document.body.classList.add('dark-mode');
+    }
+    toggleDarkmodeIcons();
+}
+
+function setCopyright() {
+    const footer = document.getElementById('footer');
+    const currentYear = new Date().getFullYear();
+    const startYear = 2022;
+    const name = 'Nicolò Taormina';
+    footer.textContent = `\u00A9 ${startYear}-${currentYear} ${name}`;
+}
+
+// Evento "DOMContentLoaded"
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle-button');
+
+    setInitialTheme(); // Imposta il tema iniziale
+
+    darkModeToggle.addEventListener('click', toggleTheme);
+});
